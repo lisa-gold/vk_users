790f8fc3dec4df9980c4ec17f3eec381ade923e9
Elizaveta <133124668+lisa-gold@users.noreply.github.com>
2024-03-23 13:44:51 +0300

    Update search_parameters.py

--- vk_users/search_parameters.py
+++ vk_users/search_parameters.py
@@ -1,4 +1,4 @@
-CITY_ID = 1  # change this field to add users from other cities
+CITY_ID = 99  # change this field to add users from other cities
 FILTER_VALUE = ['']  # filter value (names)
 CITY = 'Москва'  # change this field to add other cities to the db
 YEAR_START = 1965
